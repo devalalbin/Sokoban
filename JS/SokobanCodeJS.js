926d3d85b51dd879c7155d259d6af40c8844ce86
devalalbin <vala@student.chalmers.se>
2021-11-07 19:48:22 +0100

    Game logic working, just missing player image

--- JS/SokobanCodeJS.js
+++ JS/SokobanCodeJS.js
@@ -1,25 +1,7 @@
-function keyPress()
-    {
-        let el = document.getElementsByName("inputfield2")[0];
-        el.classList.add("MyCssClass");
-        alert("you added a class to inputfield2 when you pushed a button");
-    }
-
-function validateInput()
-{
-    
-    let formValue = document.forms["myForm"]["myFormTextInput"].value;
-    if(formValue === "")
-    {
-        document.getElementById("errorMessage").innerHTML = "Error: input needs a value";
-    }
-    else{
-        document.getElementById("errorMessage").innerHTML = "";
-
-        // Post data to backend..
-    }
-}
-
+var playerposX = 11;
+var playerposY = 11;//starting pos for player
+var img = document.createElement("img");
+img.src = "player.png";
 
 /*a function to generate the elements that will represent the tiles from the 
 map array*/
@@ -39,8 +21,7 @@ function drawMap(tileMap){
                 /* Map each entity to a color*/
                 temp=document.createElement('div');
                 temp.className = 'wall';
-                temp.innerHTML = "wall";
-                document.getElementById('map').append(temp);
+                
             
             }
             else if (tileMap.mapGrid[x][y]== "B"){
@@ -48,35 +29,35 @@ function drawMap(tileMap){
                 temp=document.createElement('div');
                 temp.className = 'block';
                 temp.innerHTML = "block";
-                document.getElementById('map').append(temp);
+                
                
             }else if (tileMap.mapGrid[x][y]== "P"){
                 console.log("PLAYER");
                 temp=document.createElement('div');
                 temp.className = 'player';
                 temp.innerHTML = "player";
-                document.getElementById('map').append(temp);
+                temp.id = "x"+x+"y"+y;
+            
+                
                 
             }
             else if (tileMap.mapGrid[x][y]== "G"){
                 console.log("GOAL");
                 temp=document.createElement('div');
                 temp.className = 'goal';
-                temp.innerHTML = "goal";
-                document.getElementById('map').append(temp);
+                
                
             }
             
             else if (tileMap.mapGrid[x][y]== " "){
                 console.log("EMPTY SPACE");
                 temp=document.createElement('div');
-                temp.className = 'empty';
-                temp.innerHTML = "";
-                document.getElementById('map').append(temp);
+                
                 
             }
 
-            
+            temp.id = "x"+x+"y"+y; /*sets id of class with their tile location */
+            document.getElementById('map').append(temp);
 
         }
         
@@ -84,38 +65,84 @@ function drawMap(tileMap){
 }
 
 
-function gridPosTest(tileMap){
-    console.log(tileMap.gridMap[0][0]);
-}
+function gameLogic(x,y){ /* takes player class and spot it want to move to and 'swaps' their id and class*/
+   //tracking 3 pos, current player pos / space you want to move pos / and space you want to move in +1
+    var oldPos = "x"+(playerposX)+"y"+(playerposY);
+    var newPosX = (playerposX +x);
+    var newPosY = (playerposY +y);
+    var newPos = "x"+(newPosX)+"y"+(newPosY);
+    var stepaheadPosX = (playerposX +x+x);//variables to check 1 step ahead
+    var stepaheadPosY = (playerposY +y+y); 
+    var stepaheadPos= "x"+(stepaheadPosX)+"y"+(stepaheadPosY);
 
+    
 
+    if(document.getElementById(newPos).className != 'wall' && document.getElementById(newPos).className != 'block' && document.getElementById(newPos).className != 'goal block'){
+         //check if new pos is a wall or block, and if its not update player pos
+        
+        document.getElementById(oldPos).innerHTML = "";
+        document.getElementById(oldPos).classList.remove("player")
+        document.getElementById(newPos).innerHTML = ("player");
+        document.getElementById(newPos).classList.add("player"); // replace block class with player in  new pos
 
-/*todo hitta gridposition för specifik element*/
+        playerposX = newPosX;
+        playerposY = newPosY;
+        
+    }
+    if(document.getElementById(newPos).className == 'block' || document.getElementById(newPos).className == 'goal block'){ //pushing block logic
+        if(document.getElementById(stepaheadPos).className != 'wall' && document.getElementById(stepaheadPos).className != 'block' && document.getElementById(stepaheadPos).className != 'goal block' ){
+            //checking position ahead to see if a push is possible
+            console.log(document.getElementsByClassName('goal block').length); //checking
+            document.getElementById(oldPos).innerHTML = "";
+            document.getElementById(oldPos).classList.remove("player");
 
+            document.getElementById(newPos).classList.remove("block");
+            document.getElementById(newPos).classList.add("player"); // replace block class with player in  new pos
+            document.getElementById(newPos).innerHTML = "player";
 
-/*
-function handleKey(e) {
-    switch (e.keyCode) {
-      case keys.left:
-        position.y--;
-        break;
-      case keys.up:
-        position.x--;
-        break;
-  
-      case keys.right:
-        position.y++;
-        break;
-  
-      case keys.down:
-        position.x++;
-        break;
-    }}*/
+            document.getElementById(stepaheadPos).innerHTML = "block";
+            document.getElementById(stepaheadPos).classList.add("block");
+
+            playerposX = newPosX;
+            playerposY = newPosY;
+            
+        }
+   
+    }    
+    winCondition();     
+
+};
+
+window.onscroll = () => { window.scroll(0, 0); }; // makes page unscrollable
+
+function winCondition(){
+    console.log(document.getElementsByClassName('goal block').length); //checking how many "goal blocks" we have
+    if(document.getElementsByClassName('goal block').length == 6){
+        alert("YOU WON! Congratulations");
+    }
+
+};
+
 
 document.addEventListener('keyup', logKey)
 function logKey(e)
 {
-    console.log(e.key);
+    e.preventDefault()
+    switch (e.key) {
+        case 'ArrowLeft':
+            gameLogic(0,-1);
+            break;
+        case 'ArrowRight':
+            gameLogic(0,1);
+            break;
+        case 'ArrowUp':
+            gameLogic(-1,0);
+            break;
+        case 'ArrowDown':
+            gameLogic(1,0);
+            break;
+    }
     
 };
+
 drawMap(tileMap01);
